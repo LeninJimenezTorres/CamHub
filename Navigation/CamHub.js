e704625534bdacb92564d0ded3ca648ebf1df200
LeninJimenezTorres <jlenin25@hotmail.com>
2022-02-23 13:05:31 -0500

    Last commit - Fail permissions management

--- Navigation/CamHub.js
+++ Navigation/CamHub.js
@@ -1,13 +1,87 @@
-import { View, StyleSheet, Text, Image, SafeAreaView, TouchableOpacity, Alert} from 'react-native';
+import { View, StyleSheet, Text, Image, SafeAreaView, TouchableOpacity, Alert, AppState} 
+        from 'react-native';
 import { Camera } from 'expo-camera';
 import React, { useState, useEffect, useRef } from 'react';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { StorageAccessFramework } from 'expo-file-system';
+import * as Location from 'expo-location';
+
+let count = 0;
+
+const get = async () => {
+  console.log('get permissions');
+  try {
+    return await MediaLibrary.getPermissionsAsync();
+  } catch (err) {
+    throw err;
+  }
+};
+
+const request = async () => {
+  console.log('request permissions');
+  try {
+    return await MediaLibrary.requestPermissionsAsync();
+  } catch (err) {
+    throw err;
+  }
+};
 
 const CamHub = ({ navigation }) => {
+  console.log('Camera Open--------------------------------------------');
   const cam = useRef(null);
-  //const cam = React.createRef(null);
+  //const [isPreview, setIsPreview] = useState(false);
+
+  const [permissions, setPermissions] = React.useState(null);
+  const [isAlreadyAsked, setAsked] = React.useState(null);
+  const appState = React.useRef(AppState.currentState);
+  
+  React.useEffect(() => {
+    console.log('First time running', permissions);
+    (async () => {
+      const currentPermissions = await MediaLibrary.getPermissionsAsync();
+      if (currentPermissions.status === 'undetermined') {
+        const newPermissions = await MediaLibrary.requestPermissionsAsync();
+        setAsked(true);
+        console.log('got new permissions', newPermissions);
+        setPermissions(newPermissions);
+      }
+      console.log('Getting permissions status:', currentPermissions.granted);
+      return setPermissions(currentPermissions);
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    console.log('Action useEffect run =>', count++);
+    (async () => {
+      console.log('async () =>');
+      if (!permissions) return; // first time dont execute !
+      console.log('Run and check if we can ask again');
+      if (!permissions?.granted && permissions?.canAskAgain) {
+        console.log('Permission is not granted and we allow to ask again.');
+        // if the user already asked , we just get the new permissions.
+        if (isAlreadyAsked) {
+          console.log('isAlreadyAsked');
+          const newPermissions = await MediaLibrary.getPermissionsAsync();
+          console.log('getPermissionsAsync() =>');
+          return setPermissions(newPermissions);
+        }
+
+        // if the user never asked in this app life cycle we will request.
+        console.log('We prompt user and get new permissions');
+        const currentPermissions = await MediaLibrary.requestPermissionsAsync();
+        console.log('requestPermissionsAsync() =>', currentPermissions);
+        setAsked(true);
+        if (currentPermissions.granted) {
+          return setPermissions(currentPermissions);
+        }
+      } else {
+        console.log('Not allowed to Ask again', count);
+      }
+    })();
+  }, []);
 
   async function takePic(){
     if( cam ) {
@@ -22,10 +96,17 @@ const CamHub = ({ navigation }) => {
     if( cam) {
       console.log('Take a picture');
       try{
-        let photo = await cam.current.takePictureAsync({allowsEditing:true, aspect:[4,3], quality:1, exif: true});
+        const options ={allowsEditing:true, ratio:[16,9], quality:1, exif: true};//
+        let photo = await cam.current.takePictureAsync(options);
         console.log(cam.current.getSupportedRatiosAsync());
+
+        //FileSystem.makeDirectoryAsync(FileSystem.documentDirectory+'/CamHub', { intermediates: true })
+        //console.log(FileSystem.documentDirectory);
+        
         const source=photo.uri;
         if (source){
+          //await cam.current.pausePreview();
+          
           handleSave(source);
         }
         return photo;
@@ -37,20 +118,32 @@ const CamHub = ({ navigation }) => {
   }
 
   const handleSave = async (source)=>{
-    const {status} = await MediaLibrary.requestPermissionsAsync();
-    if (status==="granted"){
-      console.log("Permisos concedidos");
-      await MediaLibrary.createAssetAsync(source);
-      console.log("Foto guardada");
-    }
-    else{
-      console.log("No concede permisos");
-    }
-  };
+      //const permisoAlbum = await MediaLibrary.requestPermissionsAsync();
+      const permisoAlbum = await MediaLibrary.requestPermissionsAsync();
+      console.log('Estado del permiso: '+permisoAlbum.status);
+      if (permisoAlbum.status='granted'){
+        console.log("Funcion guardar");
+        const asset = await MediaLibrary.createAssetAsync(source);
+        if (asset){
+          console.log("Asset created");
+          //await MediaLibrary.saveToLibraryAsync(asset);
+          await MediaLibrary.createAlbumAsync('CamHub',asset, false);
+          //await MediaLibrary.addAssetsToAlbumAsync(asset, 'CamHub', false)
+          //await FileSystem.deleteAsync(asset.uri)
+          //await MediaLibrary.deleteAssetsAsync(asset);
+          console.log("Foto guardada");
+        }
+        else {console.log("Asset not created")};
 
+      }
+      else{
+        auxA=false;
+        console.log("No se pudo completar la operacion por falta de permisos");
+      }
+  };
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.front);
-
+  
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
@@ -63,6 +156,7 @@ const CamHub = ({ navigation }) => {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
+  
 
   return (
     <SafeAreaView style={{width:'100%', height:'100%', backgroundColor:'rgba(0,0,250,0)'}}>
---
+++ README.md
@@ -0,0 +1,5 @@
+Camera App - React Native Expo
+
+This React Native App use Expo to builda a camera application.
+
+The progress has been stopped but some problems with expo permissions to file system, so that this project continues with React Native CLI in the next repository.
